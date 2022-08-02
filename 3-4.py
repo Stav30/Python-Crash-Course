e57d5b0560b8b9cbd9c945db4f95810c5d5813c4
Stavros <srammos1@gmail.com>
2022-08-02 16:32:27 -0700

    Solved 3-4.py
    3 person dinner guest list w print message including text and guests

---
+++ 3-4.py
@@ -0,0 +1,5 @@
+# Guest List
+dinner_guests = ['Gauss', 'Archimedes','Socrates']
+print(f"{dinner_guests[0]} is one of my dinner guests")
+print(f"{dinner_guests[1]} is one of my dinner guests")
+print(f"{dinner_guests[2]} is one of my dinner guests")
