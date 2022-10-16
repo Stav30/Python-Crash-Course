84030be2adc80fd81d6cbf9e5aad2ebdd5b45b47
Stavros <srammos1@gmail.com>
2022-10-16 12:15:39 -0700

    Create 8-3.py

---
+++ 8-3.py
@@ -0,0 +1,6 @@
+def make_shirt(size , message):
+	"""Display size and message """
+	print(f"\nthe shirt size is = {size}")
+	print(f"the shirt writes : {message}")
+
+make_shirt(size ='medium', message='I love the beach')
