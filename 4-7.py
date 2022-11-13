e4bf181e9d49217615ca3c2fd75bc83c04351063
Stavros <srammos1@gmail.com>
2022-11-13 16:58:15 -0700

    Create 4-7.py
    make a list of the multiples of 3 from 3 to 30. Use a for loop to print the numbers in the list.

---
+++ 4-7.py
@@ -0,0 +1,6 @@
+#4-7
+# make a list of the multiples of 3 from 3 to 30. Use a for loop to print the numbers in the list.
+numbers = []
+for number in range(3,31,3):
+	numbers.append(number)
+print(numbers)
