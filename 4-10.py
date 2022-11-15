01a14f8820f6ce12cb61852f6e09a11c1675c826
Stavros <srammos1@gmail.com>
2022-11-14 17:44:13 -0700

    Create 4-10.py
    Printing Slices

---
+++ 4-10.py
@@ -0,0 +1,11 @@
+#4-10: Slices
+# print the first 3 items in the list are
+my_foods = ['pizza', 'falafel', 'carrot cake', 'canoli', 'ice cream']
+print("The first 3 items are : ")
+print(my_foods[:3])
+#print the middle 3 items are
+print("The middle 3 items are : ")
+print(my_foods[1:4])
+#print The last 3 items are
+print("The last 3 items are: ")
+print(my_foods[2:5])
